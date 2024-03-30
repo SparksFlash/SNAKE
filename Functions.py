15f3364d717b11ef37ad9080f4490fb12cea6c75
Sandipto Saha <133910106+SparksFlash@users.noreply.github.com>
2024-03-30 23:57:51 +0600

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,11 @@
+def greet(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
+    print("Welcome to Bangladesh")
+
+
+greet("Alex", "Asim")
+
+
+# Two types of function -
+# 1 Perform a task
+# 2 Return a value
